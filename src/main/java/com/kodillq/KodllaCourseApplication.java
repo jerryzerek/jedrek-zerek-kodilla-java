e1ee3ec53e98c441ee14ea25940fbe14f7b52fad
Jedrzej Zerek <Jedrzej.Zerek@pl.ey.com>
2019-01-07 19:14:01 +0100

    Task 13.1

---
+++ src/main/java/com/kodillq/KodllaCourseApplication.java
@@ -0,0 +1,14 @@
+package com.kodillq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodllaCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodllaCourseApplication.class, args);
+    }
+
+}
+
